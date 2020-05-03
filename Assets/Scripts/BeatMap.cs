48f9a4b645b4a59a95c50171725766cf73be6123
Bryan <kirbyderby2000@gmail.com>
2020-05-02 23:04:22 -0700

    Added the gameplay prototype scene to the repository. Also added all of the prototype resources as well. Please refer to the "Prototype Scene" in the "Scenes" folder for the prototype scene. You may adjust the player controller settings through the "Player Controller" script in the inspector (gravity, jump height, movement speed, etc).  You may also adjust the crosshair, image as well.
    Link to the FPS Hands: https://www.blendswap.com/blend/11725
    
    Link to the Weapons: http://devassets.com/assets/sci-fi-weapons/
    
    Link to the Crosshair: https://www.kenney.nl/assets/crosshair-pack
    
    You may also start creating beatmaps already. Click on "Create" -> "Beat Map" and enter all of your beat map data. Hover over all of the beat map variables for useful tooltips.

---
+++ Assets/Scripts/BeatMap.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Beat Map", menuName ="Beat Map")]
+public class BeatMap : ScriptableObject
+{
+    [Header("Track Data")]
+    [Tooltip("The audio track")]
+    [SerializeField] AudioClip musicTrack;
+    [Tooltip("The audio track beat timestamps in seconds")]
+    [SerializeField] List<float> beatTimestamps;
+    [Tooltip("The audio track's beats per minute")]
+    [SerializeField] float BPM;
+    [Header("Loop Settings")]
+    [Tooltip("Whether or not this audio track should use a custom starting timestamp to loop")]
+    [SerializeField] bool useCustomStartLoopTimestamp = false;
+    [Tooltip("The timestamp in seconds of the song where it should begin looping")]
+    [SerializeField] float customLoopableStartTimestamp = 0.0f;
+    [Tooltip("Whether or not this audio track should use a custom ending timestamp to loop")]
+    [SerializeField] bool useCustomEndLoopTimestamp = false;
+    [Tooltip("The timestamp of the song where it should end looping")]
+    [SerializeField] float customLoopableEndTimestamp = 0.0f;
+
+    /// <summary>
+    /// The music track
+    /// </summary>
+    public AudioClip MusicTrack
+    {
+        get { return musicTrack; }
+    }
+
+    /// <summary>
+    /// Returns the beat map timestamps
+    /// </summary>
+    public List<float> BeatTimeStamps
+    {
+        get { return beatTimestamps; }
+    }
+
+    /// <summary>
+    /// Returns the beats per minute
+    /// </summary>
+    public float BeatsPerMinute
+    {
+        get
+        {
+            return BPM;
+        }
+    }
+
+    /// <summary>
+    /// The music track's duration
+    /// </summary>
+    public float TrackDuration
+    {
+        get
+        {
+            return MusicTrack.length;
+        }
+    }
+
+    /// <summary>
+    /// The custom loopable starting timestamp 
+    /// </summary>
+    public float CustomLoopableStartTimestamp
+    {
+        get
+        {
+            if (customLoopableStartTimestamp < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return customLoopableStartTimestamp;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns whether or not this audio track has a custom loopable begining
+    /// </summary>
+    public bool UseCustomLoopStartTimestamp
+    {
+        get { return useCustomStartLoopTimestamp; }
+    }
+
+    /// <summary>
+    /// Returns whether or not this audio track has a custom loopable end
+    /// </summary>
+    public bool UseCustomLoopEndTimestamp
+    {
+        get { return useCustomEndLoopTimestamp; }
+    }
+
+    /// <summary>
+    /// The custom loopable ending timestamp 
+    /// </summary>
+    public float CustomLoopableEndTimestamp
+    {
+        get
+        {
+            if(customLoopableEndTimestamp > musicTrack.length)
+            {
+                return musicTrack.length;
+            }
+            else
+            {
+                return customLoopableEndTimestamp;
+            }
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Game Objects")]
+    [SerializeField] CharacterController playerController;
+    [SerializeField] Camera playerCamera;
+    [SerializeField] Transform groundCheckReference;
+
+    [Header("Player Movement Settings")]
+    [SerializeField] float movementSpeed;
+    [SerializeField] float playerJumpHeight = 2.0f;
+    
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] LayerMask groundLayerMask;
+
+    [Header("Unity Input Settings")]
+    [SerializeField] string horizontalMove;
+    [SerializeField] string verticalMove;
+    [SerializeField] string lookXAxis;
+    [SerializeField] string lookYAxis;
+    [SerializeField] bool invertYAxis;
+    [SerializeField] string jump;
+
+
+    /// <summary>
+    /// The player's velocity
+    /// </summary>
+    Vector3 playerVelocity = new Vector3();
+
+    /// <summary>
+    /// The player's Y rotation (the rotation of the player's camera)
+    /// </summary>
+    Vector3 playerCameraRotation;
+
+    /// <summary>
+    /// Indicator used if the player is on the ground
+    /// </summary>
+    private bool isGrounded = false;
+
+    private void Awake()
+    {
+        // Set the mouse curser so it's locked
+        Cursor.lockState = CursorLockMode.Locked;
+        // Cache the player's initial camera rotation
+        playerCameraRotation = playerCamera.transform.localEulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessMouseInput();
+        ProcessMoveInput();
+        UpdateAndApplyVelocity();
+    }
+
+    /// <summary>
+    /// Method called to process the player's mouse input (look)
+    /// </summary>
+    private void ProcessMouseInput()
+    {
+        // Get the player  game object rotation
+        Vector3 playerTransformRotation = playerController.transform.eulerAngles;
+        // Rotate the object y axis by the mouse x axis input
+        playerTransformRotation.y += Input.GetAxis(lookXAxis);
+        // Apply the player input rotation
+        playerController.transform.eulerAngles = playerTransformRotation;
+        // Get the mouse y axis input
+        float mouseYInput = Input.GetAxis(lookYAxis);
+        // Invert it if required
+        if (invertYAxis) mouseYInput *= -1.0f;
+        // Get the player camera rotation, add the mouse input to it
+        playerCameraRotation.x = playerCameraRotation.x + mouseYInput;
+        // Clamp the rotation so it's between -90 and 90 degrees
+        playerCameraRotation.x = Mathf.Clamp(playerCameraRotation.x, -90.0f, 90.0f);
+        // Apply its rotation
+        playerCamera.transform.localEulerAngles = playerCameraRotation;
+    }
+
+    /// <summary>
+    /// Method called to process the player's movement input
+    /// </summary>
+    private void ProcessMoveInput()
+    {
+        // Get the horizontal and vertical movement
+        float x = Input.GetAxis(horizontalMove);
+        float z = Input.GetAxis(verticalMove);
+        // Get the input relative to the player's current position (player's forward and players right)
+        Vector3 movement = playerController.transform.forward * z + playerController.transform.right * x;
+        // Adjust the movement vector by speed and deltaTime
+        movement *= movementSpeed * Time.deltaTime;
+        playerController.Move(movement);
+    }
+
+    /// <summary>
+    /// Updates and applies the player's velocity
+    /// </summary>
+    private void UpdateAndApplyVelocity()
+    {
+        // Cache whether or not the player is grounded
+        isGrounded = IsGrounded();
+        // If the player is grounded, then allow the player the jump or reset the velocity
+        if (isGrounded)
+        {
+            Debug.Log("Grounded");
+            // Allow the player to jump
+            Jump();
+            // If the player didn't jump, then keep reseting the velocity to -3
+            if(playerVelocity.y < 0)
+            {
+                playerVelocity.y = -3.0f;
+            }
+        }
+        // If the player is airborne, then keep applying the gravity to the player's velocity
+        else
+        {
+            Debug.Log("Airborne");
+            playerVelocity.y += gravity * Time.deltaTime;
+        }
+        // Apply the player's velocity
+        playerController.Move(playerVelocity * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Applies a jump
+    /// </summary>
+    private void Jump()
+    {
+        // If the player is jumping, set the velocity upwards
+        // The formula for jumping at a specific height is
+        // Square Root of (desiredHeigh * -2 * gravity)
+        if (Input.GetButtonDown(jump))
+        {
+            playerVelocity.y = Mathf.Sqrt(playerJumpHeight * -2.0f * gravity);
+        }
+    }
+
+    /// <summary>
+    /// Whether or not the player is grounded
+    /// </summary>
+    /// <returns></returns>
+    bool IsGrounded()
+    {
+        return Physics.CheckBox(groundCheckReference.position, groundCheckReference.transform.lossyScale, Quaternion.identity, groundLayerMask.value);
+    }
+
+
+}
