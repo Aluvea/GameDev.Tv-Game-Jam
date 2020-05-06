bd98d1cb5be74e287b68f3dafd1cd204dc09dc83
Aluvea <christine.truong@hotmail.ca>
2020-05-06 00:55:18 -0600

    Edited the Prototype Scene: added prefab lab rooms, added raycast element for shooting mechanic, added enemy prefab and chase range.

---
+++ Assets/Scripts/WeaponShooting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponShooting : MonoBehaviour
+{
+
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CanShoot())
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+
+    }
+
+    bool CanShoot()
+    {
+        return true;
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+        Debug.Log("I hit the " + hit.transform.name + "!");
+    }
+}
