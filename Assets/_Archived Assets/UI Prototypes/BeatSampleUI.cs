6ce50faea3bcb9bbbef2b7519f3ce1d07dbb04b7
Bryan <kirbyderby2000@gmail.com>
2020-05-14 11:15:17 -0700

    Cleaning up our Github repository by creating an Archive folder in the root. Moved all of the previous UI Prototype scenes / prefabs / scripts into it. Feel free to move any other files we don't need anymore into this folder.

---
+++ Assets/_Archived Assets/UI Prototypes/BeatSampleUI.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatSampleUI : MonoBehaviour
+{
+    private CanvasGroup uiCanvasGroup;
+
+    [SerializeField] UnityEngine.UI.RawImage innerCircle;
+    [SerializeField] UnityEngine.UI.RawImage outerCircle;
+
+
+
+    public bool PlayingBeat
+    {
+        private set;
+        get;
+    } = false;
+
+    /// <summary>
+    /// The beat sync that this beat sample UI is playing
+    /// </summary>
+    public BeatSyncData PlayingBeatSync
+    {
+        private set;
+        get;
+    }
+
+    [Range(0.0f,5.0f)]
+    public float timeToLiveAfterBeatTargetTime = 0.0f;
+    
+
+
+    private void Awake()
+    {
+        uiCanvasGroup = GetComponent<CanvasGroup>();
+    }
+    
+    public void AnimateBeatData(BeatSyncData beatData, Vector3 destination)
+    {
+        if (uiCanvasGroup == null) uiCanvasGroup = GetComponent<CanvasGroup>();
+
+        if (PlayingBeat)
+        {
+            Debug.LogError("Already playing a beat!");
+            return;
+        }
+
+        PlayingBeatSync = beatData;
+        uiCanvasGroup.alpha = 0.0f;
+        this.gameObject.SetActive(true);
+        StartCoroutine(AnimateBeatCoroutine(beatData, destination));
+    }
+
+    IEnumerator AnimateBeatCoroutine(BeatSyncData beatData, Vector3 destination)
+    {
+        PlayingBeat = true;
+        float endBeatAnimationTime = beatData.BeatTargetTime + timeToLiveAfterBeatTargetTime;
+        float lerpAMT = 0.0f;
+
+        float startTimestamp = Time.time;
+        float animationDuration = beatData.BeatTargetTime - Time.time;
+        uiCanvasGroup.alpha = 0.0f;
+        Vector3 startingPosition = transform.position;
+        while(lerpAMT < 1.0f)
+        {
+            lerpAMT = (Time.time - startTimestamp) / animationDuration;
+            transform.position = Vector3.LerpUnclamped(startingPosition, destination, lerpAMT);
+
+            uiCanvasGroup.alpha = lerpAMT;
+
+            yield return null;
+        }
+
+        Debug.Log("UI Beat!");
+
+        float fadeOutTimestamp = Time.time;
+        float fadeOutDuration = endBeatAnimationTime - 0.25f - Time.time ;
+
+        while (Time.time < endBeatAnimationTime)
+        {
+            lerpAMT = (Time.time - startTimestamp) / animationDuration;
+            transform.position = Vector3.LerpUnclamped(startingPosition, destination, lerpAMT);
+            uiCanvasGroup.alpha = Mathf.Lerp(1.0f, 0.0f, (Time.time - fadeOutTimestamp) /fadeOutDuration);
+            yield return null;
+        }
+
+
+        PlayingBeat = false;
+
+        BeatSampleUIManager_Version01.OnBeatSampleUIAnimationEnded(this);
+        
+    }
+
+
+    public virtual void SetBeatSampleColor(Color innerCircleColor, Color outerCircleColor)
+    {
+        innerCircle.color = innerCircleColor;
+        outerCircle.color = outerCircleColor;
+    }
+
+
+}
---
+++ Assets/_Archived Assets/UI Prototypes/BeatSampleUIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class used for a beat sample ui manager
+/// </summary>
+public abstract class BeatSampleUIManager : MonoBehaviour
+{
+    /// <summary>
+    /// Base method that needs to be overwritten for a beat sample UI manager to display a beat
+    /// </summary>
+    /// <param name="beatSyncData">The beat sync data</param>
+    public abstract void DisplayBeat(BeatSyncData beatSyncData);
+
+    /// <summary>
+    /// Displays when the user inputs an action but misses a beat completely
+    /// </summary>
+    public abstract void DisplayInputMissedBeat();
+
+
+    /// <summary>
+    /// Updates the sync state of a beat sample UI
+    /// </summary>
+    /// <param name="beatHit">The beat hit</param>
+    /// <param name="beatInput">The hit sync type</param>
+    public abstract void UpdateBeatSyncUIState(BeatSyncData beatHit, BeatInputSync beatInput);
+}
+
+
