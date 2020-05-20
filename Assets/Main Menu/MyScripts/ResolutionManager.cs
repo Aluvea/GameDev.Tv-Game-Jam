4e90993454e653bd1a4f3da40f4dcf0fba43d432
olee403 <oscarlee403@gmail.com>
2020-05-20 14:22:38 -0600

    Created the ideal main menu that I first visualized with working 3D visualizers. contains the same menu setup as the first menu with working buttons with the exception of sound effect slider. Button templates are in place for starting the game and restarting the game. Also created template of game over screen with working exit to desktop button in place.

---
+++ Assets/Main Menu/MyScripts/ResolutionManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionManager : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    public void Setwidth(int newWidth)
+    {
+        width = newWidth;
+    }
+    public void SetHeight(int newHeight)
+    {
+        height = newHeight;
+    }
+    public void SetRes()
+    {
+        Screen.SetResolution(width, height, false);
+    }
+}
+
