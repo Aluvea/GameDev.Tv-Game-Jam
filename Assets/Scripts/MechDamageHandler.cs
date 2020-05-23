a22d30f0b7abc9deeeded842874fcbe47b300ecc
Bryan <kirbyderby2000@gmail.com>
2020-05-23 13:16:20 -0700

    Forgot to push the mech damage handler script

---
+++ Assets/Scripts/MechDamageHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechDamageHandler : EnemyDamageHandler
+{
+    bool attackingPlayer = false;
+
+    public override void OnDamageTaken()
+    {
+        if(attackingPlayer == false)
+        {
+            attackingPlayer = true;
+            GetComponent<MechAttackScript>().StartAttackingTarget(PlayerController.PlayerCamera.transform);
+        }
+    }
+}
