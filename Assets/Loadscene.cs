b274392cd4aad146e6cbe184e90285ad6c112304
olee403 <oscarlee403@gmail.com>
2020-05-24 16:07:50 -0600

    fixed main menu to load game properly, moved old menu to another folder to clean up current useable scenes. game over ui will now restart the game when button is pressed.

---
+++ Assets/Loadscene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loadscene : MonoBehaviour
+{
+    public void Restartgame()
+    {
+        SceneManager.LoadScene("Playtest Scene 2 - Aluvea");
+    }
+
+}
