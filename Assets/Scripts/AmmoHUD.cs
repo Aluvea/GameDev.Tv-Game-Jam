690d3bbef3e632e31c2662c701c9363de6e74533
Bryan <kirbyderby2000@gmail.com>
2020-05-31 03:01:19 -0700

    - Making possible ammo setings to use???? (maybe we can use this? Doubt I'll be able to make a reload animation in time before the deadline but who knows!) - Note: The ammo scripts aren't actually being used, I just wrote the base scripts for an ammo system - Added VFX / SFX to the health pack

---
+++ Assets/Scripts/AmmoHUD.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoHUD : MonoBehaviour
+{
+    [SerializeField] GameObject ammoImageRef;
+    [SerializeField] TMPro.TextMeshProUGUI ammoText;
+
+    [SerializeField] bool toggleAmmoHUD = false;
+
+
+    private void Awake()
+    {
+        ToggleAmmoHUD(toggleAmmoHUD);
+    }
+
+    public void ToggleAmmoHUD(bool toggle)
+    {
+        ammoImageRef.SetActive(toggle);
+        ammoText.gameObject.SetActive(toggle);
+    }
+
+    public void UpdateAmmoText(int ammoCount)
+    {
+        ammoText.text = "Ammo x " + ammoCount;
+    }
+
+
+}
---
+++ Assets/Scripts/AmmoManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoManager : MonoBehaviour
+{
+    [Header("Reload Animation References")]
+    [SerializeField] Animator reloadAnimator;
+    [SerializeField] string reloadAnimationTriggerName = "reload";
+    [Header("Ammo Settings")]
+    [Min(10)]
+    [SerializeField] int maxClipAmmoCount = 10;
+    [Min(0.5f)]
+    [SerializeField] float perfectReloadSpeed = 1.0f;
+    [Min(0.5f)]
+    [SerializeField] float goodReloadSpeed = 0.78f;
+    [Min(0.5f)]
+    [SerializeField] float okReloadSpeed = 0.60f;
+    [Min(0.5f)]
+    [SerializeField] float missReloadSpeed = 0.5f;
+    private AmmoHUD ammoHud;
+    private int currentClipCount;
+
+    /// <summary>
+    /// Whether or not the player can shoot (this is false if the player is reloading or has no ammo loaded in the clip)
+    /// </summary>
+    public bool CanShoot
+    {
+        get
+        {
+            if (currentClipCount <= 0) return false;
+            if (reloading) return false;
+            return true;
+        }
+    }
+
+
+    private void Awake()
+    {
+        ammoHud = FindObjectOfType<AmmoHUD>();
+        ammoHud.ToggleAmmoHUD(true);
+        currentClipCount = maxClipAmmoCount;
+    }
+
+    private bool reloading = false;
+
+    /// <summary>
+    /// Method called to reload
+    /// </summary>
+    public void Reload()
+    {
+        // If a reload animation is already playing then do nothing
+        if (reloading) return;
+        // Otherwise, start reloading
+        reloading = true;
+        // Play the reload animation
+        //...
+        // Request an input action
+        BeatSyncReceiver.BeatReceiver.RequestInputAction(out BeatInputSync inputSync);
+        float reloadSpeed = 1.0f;
+        switch (inputSync)
+        {
+            case BeatInputSync.PERFECT:
+                reloadSpeed = perfectReloadSpeed;
+                break;
+            case BeatInputSync.GOOD:
+                reloadSpeed = goodReloadSpeed;
+                break;
+            case BeatInputSync.OK:
+                reloadSpeed = okReloadSpeed;
+                break;
+            case BeatInputSync.MISS:
+                reloadSpeed = missReloadSpeed;
+                break;
+            default:
+                break;
+        }
+        // Adjust the reload animation speed based on the input sync
+        reloadAnimator.speed = reloadSpeed;
+        reloadAnimator.SetTrigger(reloadAnimationTriggerName);
+        // Callback reload animation finished!!!!
+    }
+
+    /// <summary>
+    /// Method called when the reload animation finishes
+    /// </summary>
+    public void ReloadAnimationFinished()
+    {
+        // Reset the current clip count to the max clip ammo count
+        currentClipCount = maxClipAmmoCount;
+        // Update the ammo HUD
+        ammoHud.UpdateAmmoText(currentClipCount);
+        // set reloading to false
+        reloading = false;
+    }
+
+    /// <summary>
+    /// Method called to dispense ammo
+    /// </summary>
+    public void DispenseAmmo()
+    {
+        // Decrement the current ammo  count
+        currentClipCount--;
+        // If the current ammo clip is less than or equal to 0, set it to 0 and set CanShoot to false (No ammo in clip)
+        if (currentClipCount <= 0)
+        {
+            currentClipCount = 0;
+        }
+        // Update the ammo text HUD
+        ammoHud.UpdateAmmoText(currentClipCount);
+    }
+}
