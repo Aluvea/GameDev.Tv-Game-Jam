d74d29023a10ebabb4cb9fa2932918dccd1596ad
Bryan <kirbyderby2000@gmail.com>
2020-05-31 13:47:32 -0700

    - Added ammo and reloading - To exclude ammo from being used, just delete the AmmoManager script from the FPS Arms game object (childed under the player prefab) - Here's how the ammo system works: - The ammo manager will always reload the magazine clip with a default number of bullets, if the player reloads with a combo count higher than the default magazine clip size, then the player will reload and be rewarded with a magazine clip size equal to the count of their combo - The reload action synchronization also makes the reload animation faster or slower depending on how in-sync the reload action is

--- Assets/Scripts/AmmoHUD.cs
+++ Assets/Scripts/AmmoHUD.cs
@@ -6,9 +6,12 @@ public class AmmoHUD : MonoBehaviour
 {
     [SerializeField] GameObject ammoImageRef;
     [SerializeField] TMPro.TextMeshProUGUI ammoText;
-
+    [SerializeField] TMPro.TextMeshProUGUI comboBonusText;
+    [Min(5.0f)]
+    [SerializeField] float comboBonusDisplayDuration = 10.0f;
     [SerializeField] bool toggleAmmoHUD = false;
 
+    
 
     private void Awake()
     {
@@ -19,12 +22,30 @@ public class AmmoHUD : MonoBehaviour
     {
         ammoImageRef.SetActive(toggle);
         ammoText.gameObject.SetActive(toggle);
+        comboBonusText.gameObject.SetActive(toggle);
     }
 
     public void UpdateAmmoText(int ammoCount)
     {
         ammoText.text = "Ammo x " + ammoCount;
     }
+    Coroutine comboBonusCoroutine = null;
+    public void PlayComboBonusAnimation(int comboBonus)
+    {
+        comboBonusText.text = "Combo Bonus + " + comboBonus;
+        if (comboBonusCoroutine != null) StopCoroutine(comboBonusCoroutine);
+        comboBonusCoroutine =  StartCoroutine(ShowComboBonusText());
+    }
+
+    IEnumerator ShowComboBonusText()
+    {
+        comboBonusText.GetComponent<Animator>().SetBool("visible", true);
+        yield return new WaitForSeconds(comboBonusDisplayDuration);
+        comboBonusText.GetComponent<Animator>().SetBool("visible", false);
+        comboBonusCoroutine = null;
+    }
+
+
 
 
 }
