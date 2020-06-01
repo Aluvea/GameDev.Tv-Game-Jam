72a2b04131b04e0d3fc5eded0b294c58b506e4af
olee403 <oscarlee403@gmail.com>
2020-05-31 18:32:27 -0600

    Exit to main menu is now working properly . Adjusted camera issue when changing resolutions ( Thanks AFK ).

---
+++ Assets/ExittoMain.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExittoMain : MonoBehaviour
+
+{
+    public void RestartGame()
+
+
+    {
+
+        SceneManager.LoadScene("Main menu (Current)", LoadSceneMode.Single);
+        Time.timeScale = 1;
+
+
+    }
+
+}
