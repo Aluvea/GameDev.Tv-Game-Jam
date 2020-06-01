f9acab0b16662d836c801493769846ba2f8307f1
Bryan <kirbyderby2000@gmail.com>
2020-05-31 18:36:50 -0700

    - Setting up the boss trigger script so it enables the boss upon contact

--- Assets/Scripts/GlassDoorsMove.cs
+++ Assets/Scripts/GlassDoorsMove.cs
@@ -8,7 +8,8 @@ public class GlassDoorsMove : MonoBehaviour
     [SerializeField] GameObject innerGlassDoor;
     [SerializeField] Animator outerAnimator;
     [SerializeField] Animator innerAnimator;
-
+    [SerializeField] BossMech bossMech;
+    [SerializeField] AIRoamingController roamingController;
     private bool open = false;
     private bool close = false;
 
@@ -26,14 +27,24 @@ public class GlassDoorsMove : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.name == "Player Prefab - Bryans Copy")
+        if(other.GetComponent<PlayerHealth>() != null)
         {
             // Play animation to Close outerGlassDoor
             outerAnimator.SetBool("close", true);
             innerAnimator.SetBool("open", true);
 
             // Play animation to Open innerGlassDoor
+            EnableBoss();
+            Destroy(this.gameObject);
         }
+    }
 
+    private void EnableBoss()
+    {
+        if(bossMech != null && roamingController != null)
+        {
+            bossMech.enabled = true;
+            roamingController.enabled = true;
+        }
     }
 }
