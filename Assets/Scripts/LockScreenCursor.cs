eafffeb871f58bb0eefeaa28ec74ace8d4e7e75b
Aluvea <christine.truong@hotmail.ca>
2020-05-31 23:38:38 -0600

    LockScreenCursor script

---
+++ Assets/Scripts/LockScreenCursor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockScreenCursor : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(LockScreenCursorCoroutine());
+    }
+
+    private IEnumerator LockScreenCursorCoroutine()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        yield return null;
+    }
+}
